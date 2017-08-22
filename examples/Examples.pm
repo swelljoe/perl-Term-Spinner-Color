0f0988fb54a1f7338608abc7d5be33bae62598f5
Joe Cooper <swelljoe@gmail.com>
2017-08-22 01:23:13 -0500

    Wrong indentation on previous perltidy run

--- examples/Examples.pm
+++ examples/Examples.pm
@@ -9,48 +9,32 @@ use Time::HiRes qw( sleep );
 use utf8;
 
 sub main {
-    my $s0 = Term::Spinner::Color->new(
-        'colorcycle' => 1,
-        'seq'        => 'uni_circle'
-    );
-    $s0->run_ok( "sleep 5", "doot circle" );
+  my $s0 = Term::Spinner::Color->new('colorcycle' => 1, 'seq' => 'uni_circle');
+  $s0->run_ok("sleep 5", "doot circle");
 
-    my $s1 = Term::Spinner::Color->new(
-        'color' => 'yellow',
-        'seq'   => [qw(dooot odoot oodot ooodt ooood ooodt oodot odoot)],
-    );
-    $s1->run_ok(
-        [
-            "sleep 0.2",
-            "sleep 0.2",
-            "sleep 0.2",
-            "sleep 0.2",
-            "sleep 0.2",
-            "false"
-        ],
-        "doot doot oh no!"
-    );
+  my $s1 = Term::Spinner::Color->new(
+    'color' => 'yellow',
+    'seq'   => [qw(dooot odoot oodot ooodt ooood ooodt oodot odoot)],
+  );
+  $s1->run_ok(
+    ["sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "false"],
+    "doot doot oh no!"
+  );
 
-    my $s2 = Term::Spinner::Color->new(
-        'colorcycle' => 1,
-        'seq'        => 'uni_dots'
-    );
-    $s2->run_ok( "sleep 5", "single doot" );
+  my $s2 = Term::Spinner::Color->new('colorcycle' => 1, 'seq' => 'uni_dots');
+  $s2->run_ok("sleep 5", "single doot");
 
-    my $s3 = Term::Spinner::Color->new();
-    $s3->run_ok( "sleep 5", "default doot" );
+  my $s3 = Term::Spinner::Color->new();
+  $s3->run_ok("sleep 5", "default doot");
 
-    my $s4 = Term::Spinner::Color->new(
-        'color' => 'magenta',
-        'seq'   => 'uni_trigram_bounce'
-    );
-    $s4->run_ok( "sleep 5", "trigram_bounce" );
+  my $s4 = Term::Spinner::Color->new(
+    'color' => 'magenta',
+    'seq'   => 'uni_trigram_bounce'
+  );
+  $s4->run_ok("sleep 5", "trigram_bounce");
 
-    my $s5 = Term::Spinner::Color->new(
-        'color' => 'green',
-        'seq'   => 'uni_dots7'
-    );
-    $s5->run_ok( "sleep 5", "uni_dots7" );
+  my $s5 = Term::Spinner::Color->new('color' => 'green', 'seq' => 'uni_dots7');
+  $s5->run_ok("sleep 5", "uni_dots7");
 }
 
-exit main( \@ARGV );
+exit main(\@ARGV);
